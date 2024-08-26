3e5e55b0f57988ca3b162d073a248a9fce6af184
PRASHANT THAKUR <prashant101007@gmail.com>
2024-08-26 11:53:33 +0530

    understood internal functioning of react a little

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App! chai</h1>
+    </div>
+  )
+
+}
+
+/*const Reactelement={
+  type:'a',
+  props:{
+    href:'https://google.com',
+    target:'_blank'
+  },
+  children:'click me to visit google'
+}*/
+const anotheruser="chai aur react"
+
+const anotherElement=(
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+//how react accepts the code in behind the scene working
+const reactElement= React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank'},
+ ' click me to open google',
+ anotheruser
+
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    reactElement
+
 )
