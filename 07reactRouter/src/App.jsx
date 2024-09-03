bba524b0ae39a18bb3869cb3079ff5f246e765f4
PRASHANT THAKUR <prashant101007@gmail.com>
2024-09-03 18:50:03 +0530

    learned react router

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/header'
+import Footer from './components/FOOTER/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='bg-green-600 p-4'>React Router</h1>
+    
+     
     </>
   )
 }
