7602c6edc37807ec0b03c4fa6dd08db57145bb93
PRASHANT THAKUR <prashant101007@gmail.com>
2024-10-30 12:30:33 +0530

    finally brought back login and signup page

--- 12MegaBlog/src/components/footer/Footer.jsx
+++ 12MegaBlog/src/components/footer/Footer.jsx
@@ -1,7 +1,9 @@
+
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Logo from '../Logo'
- function Footer() {
+
+function Footer() {
   return (
     <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
             <div className="relative z-10 mx-auto max-w-7xl px-4">
