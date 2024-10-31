8c888a321502d5886b71c21b57a77e6af18a2fcb
PRASHANT THAKUR <prashant101007@gmail.com>
2024-10-31 13:16:00 +0530

    progresed on debugging

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -14,6 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     render={({field: {onChange}}) => (
         <Editor
         initialValue={defaultValue}
+        apiKey='1r16k47nri19mo9b6pqmk1i27gn37e6un549zgyf95b7hrv7'
         init={{
             initialValue: defaultValue,
             height: 500,
